98259a9d93d45bed396f45ae28ffd0f703f2e72f
LucasMalinowski <lucasmalino@outlook.com>
2024-08-30 11:13:07 -0300

    Update name of project

--- app/models/address.rb
+++
@@ -1,8 +0,0 @@
-class Address < ApplicationRecord
-  validates :cep, :street, :neighborhood, :city, :uf, presence: true
-  validates :cep, format: { with: /\A\d{5}-\d{3}\z/, message: "Precisa ser um CEP valido" }
-  validates :uf, format: { with: /\A[A-Z]{2}\z/, message: "Precisa ser um UF valido" }
-  validates :ibge_code, numericality: { only_integer: true }, allow_blank: true
-
-  belongs_to :municipe
-end

bd25da2af2af7cb5ded70aa27a43dbbd81007a47
lucasbrazau <lucasbrazau@fetch.ly>
2025-08-30 23:48:02 -0300

    Add Pundit authorization to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index, :show]
   before_action :configure_permitted_parameters, if: :devise_controller?
